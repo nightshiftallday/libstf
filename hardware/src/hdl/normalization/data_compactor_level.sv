82ca76d9bc75fe4ace2e3ee06e6422c57328face
Si Jun Kwon <si_jun.kwon@tu-darmstadt.de>
2026-07-27 10:08:07 +0200

    Resolve merge conflict for data compactor level

--- hardware/src/hdl/normalization/data_compactor_level.sv
+++ hardware/src/hdl/normalization/data_compactor_level.sv
@@ -5,6 +5,7 @@ module DataCompactorLevel #(
     parameter type data_t,
     parameter NUM_ELEMENTS,
     parameter REGISTER = 0,
+    parameter SKID = 0,
     parameter COUNTER_WIDTH = $clog2(NUM_ELEMENTS)
 ) (
     input logic clk,
@@ -43,39 +44,59 @@ always_comb begin
     end
 end
 
-generate if (REGISTER) begin
+generate if (SKID) begin
+    
     typedef struct packed {
-        data_t[NUM_ELEMENTS - 1:0] data;
-        logic[NUM_ELEMENTS - 1:0]  keep;
-        logic                      last;
-        logic[COUNTER_WIDTH - 1:0] counter;
+        data_t [NUM_ELEMENTS - 1:0]     data;
+        logic  [NUM_ELEMENTS - 1:0]     keep;
+        logic                           last;
+        logic [COUNTER_WIDTH - 1:0]    counter;
     } stage_t;
 
-    ready_valid_i #(stage_t) skid_in(clk, rst_n), skid_out(clk, rst_n);
-
-    assign skid_in.data.data    = next_data;
-    assign skid_in.data.keep    = next_keep;
-    assign skid_in.data.last    = in.last;
-    assign skid_in.data.counter = next_counter;
-    assign skid_in.valid        = in.valid;
-    assign in.ready             = skid_in.ready;
-
-    SkidBuffer #(
-        .data_t(stage_t)
-    ) inst_skid_buffer (
+    ready_valid_i #(stage_t) skid_in (.*);
+    ready_valid_i #(stage_t) skid_out (.*);
+    SkidBuffer #(stage_t) data_compactor_skid (
         .clk(clk),
         .rst_n(rst_n),
 
         .in(skid_in),
         .out(skid_out)
     );
+    
+    always_comb begin
+        skid_in.data = '{
+            data: next_data,
+            keep: next_keep,
+            last: in.last,
+            counter: next_counter
+        };
+        skid_in.valid = in.valid;
+        in.ready = skid_in.ready;
 
-    assign out.data       = skid_out.data.data;
-    assign out.keep       = skid_out.data.keep;
-    assign out.last       = skid_out.data.last;
-    assign out.valid      = skid_out.valid;
-    assign counter_out    = skid_out.data.counter;
-    assign skid_out.ready = out.ready;
+        out.data = skid_out.data;
+        out.keep = skid_out.keep;
+        out.ready = skid_out.ready;
+        out.valid = skid_out.valid;
+        skid_out.ready = out.ready;
+    end
+
+end else if (REGISTER) begin
+    always_ff @(posedge clk) begin
+        if (!rst_n) begin
+            out.valid <= 1'b0;
+        end else begin
+            if (!out.valid || out.ready) begin
+                out.data  <= next_data;
+                out.keep  <= next_keep;
+                out.last  <= in.last;
+                out.valid <= in.valid;
+
+                counter_out <= next_counter;
+            end
+        end
+    end
+
+    assign in.ready = !out.valid || out.ready;
 end else begin
     assign out.data  = next_data;
     assign out.keep  = next_keep;
